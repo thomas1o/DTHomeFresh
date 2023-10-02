63452b2e6f81d769c94876ccb5dffccd0a7b4f83
Thomas BIju <thomasbiju43210@gmail.com>
2023-10-02 13:07:58 +0530

    Added the second activity

---
+++ app/src/main/java/com/example/dthomefresh/CategoriesActivty.kt
@@ -0,0 +1,11 @@
+package com.example.dthomefresh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoriesActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+    }
+}
--- app/src/main/java/com/example/dthomefresh/MainActivity.kt
+++ app/src/main/java/com/example/dthomefresh/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.dthomefresh
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.get_started_button)
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        button.setOnClickListener {
+            vibrator.vibrate(100)
+            val intent = Intent(this, CategoriesActivty::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
