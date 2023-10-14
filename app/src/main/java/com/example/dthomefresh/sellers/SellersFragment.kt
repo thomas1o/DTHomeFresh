8ac9a0fb5c9f429aa70e1533ba4cd7e58a2b2a63
Thomas BIju <thomasbiju43210@gmail.com>
2023-10-14 12:30:03 +0530

    SellersViewModel takes in buttonClick and processes it

--- app/src/main/java/com/example/dthomefresh/sellers/SellersFragment.kt
+++ app/src/main/java/com/example/dthomefresh/sellers/SellersFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.dthomefresh.R
@@ -18,19 +19,14 @@ import com.example.dthomefresh.databinding.FragmentSellersBinding
 class SellersFragment : Fragment() {
 
     private lateinit var viewModel: SellersViewModel
+    private lateinit var binding: FragmentSellersBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val option = mutableMapOf(
-            "food" to false,
-            "clothes" to false,
-            "homemade_items" to false
-        )
-
-        val binding: FragmentSellersBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sellers, container, false
         )
 
@@ -40,40 +36,35 @@ class SellersFragment : Fragment() {
         binding.upButton.setOnClickListener{
             Navigation.findNavController(it).navigate(R.id.action_sellersFragment_to_categoriesFragment)
         }
-        //TODO- figure out a better logic
-        binding.cardNavigation1.setOnClickListener {
-            option["food"] = true
-            if(option["food"] == true) {
+
+//        TODO- figure out a better logic
+
+        viewModel.options.observe(viewLifecycleOwner, Observer { newOptions ->
+            if(newOptions[0] == true) {
+                setDefaultColor()
                 binding.navigationText1.setTextColor(ContextCompat.getColor(requireContext(), R.color.yellow_seller_navigation))
                 binding.cardNavigation1.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_seller_navigation))
-                binding.navigationText2.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
-                binding.cardNavigation2.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
-                binding.navigationText3.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
-                binding.cardNavigation3.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
             }
-        }
-        binding.cardNavigation2.setOnClickListener {
-            option["clothes"] = true
-            if(option["clothes"] == true) {
-                binding.navigationText1.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
-                binding.cardNavigation1.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
+            else if(newOptions[1] == true) {
+                setDefaultColor()
                 binding.navigationText2.setTextColor(ContextCompat.getColor(requireContext(), R.color.yellow_seller_navigation))
                 binding.cardNavigation2.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_seller_navigation))
-                binding.navigationText3.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
-                binding.cardNavigation3.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
             }
-        }
-        binding.cardNavigation3.setOnClickListener {
-            option["homemade_items"] = true
-            if(option["homemade_items"] == true) {
-                binding.navigationText1.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
-                binding.cardNavigation1.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
-                binding.navigationText2.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
-                binding.cardNavigation2.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
+            else if(newOptions[2] == true) {
+                setDefaultColor()
                 binding.navigationText3.setTextColor(ContextCompat.getColor(requireContext(), R.color.yellow_seller_navigation))
                 binding.cardNavigation3.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_seller_navigation))
             }
-        }
+        })
+
         return binding.root
+    }
+    private fun setDefaultColor() {
+        binding.navigationText1.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
+        binding.cardNavigation1.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
+        binding.navigationText2.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
+        binding.cardNavigation2.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
+        binding.navigationText3.setTextColor(ContextCompat.getColor(requireContext(), R.color.green_dark))
+        binding.cardNavigation3.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_lighter))
     }
 }
--- app/src/main/java/com/example/dthomefresh/sellers/SellersViewModel.kt
+++ app/src/main/java/com/example/dthomefresh/sellers/SellersViewModel.kt
@@ -1,14 +1,42 @@
 package com.example.dthomefresh.sellers
 
+import android.text.BoringLayout
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SellersViewModel : ViewModel() {
 
+    private val _options = MutableLiveData<MutableMap<Int, Boolean>>()
+    val options: LiveData<MutableMap<Int, Boolean>>
+            get() = _options
     init {
-        Log.i("SellerViewModel", "viewModel is created")
+        setAllFalse()
     }
-
-
-
+    fun onFood() {
+        setAllFalse()
+        setTrue(0)
+    }
+    fun onClothes() {
+        setAllFalse()
+        setTrue(1)
+    }
+    fun onHomemadeItems() {
+        setAllFalse()
+        setTrue(2)
+    }
+    private fun setAllFalse() {
+        _options.value = mutableMapOf(
+            0 to false,
+            1 to false,
+            2 to false
+        )
+    }
+    private fun setTrue(elNum : Int) {
+        val currentOptions = _options.value
+        currentOptions?.set(elNum, true)
+        _options.value = currentOptions!!
+    }
 }
