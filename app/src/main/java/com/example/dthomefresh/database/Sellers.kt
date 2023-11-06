2c8e747b933080312b8ccfc8fa7cc3f8e4784863
Thomas BIju <thomasbiju43210@gmail.com>
2023-11-06 18:13:32 +0530

    Fixed navigation

---
+++ app/src/main/java/com/example/dthomefresh/database/Sellers.kt
@@ -0,0 +1,10 @@
+package com.example.dthomefresh.database
+
+//TODO complete these databases
+data class Sellers(
+
+    var name: String, var address: String, var contact: Long
+
+
+
+)
--- app/src/main/java/com/example/dthomefresh/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/dthomefresh/profile/ProfileFragment.kt
@@ -14,10 +14,16 @@ import com.example.dthomefresh.databinding.FragmentProfileBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.database
 
 class ProfileFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+    private val database = Firebase.database
+    private val myRef = database.getReference("Sellers")
+
+    private lateinit var name: String
+    private lateinit var contact: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +33,15 @@ class ProfileFragment : Fragment() {
         )
 
         auth = Firebase.auth
+
+        binding.saveButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
+
+            name = binding.etName.toString()
+            contact = binding.tiPhoneNumber.toString()
+
+
+        }
 
         binding.signOutButton.setOnClickListener{
             Firebase.auth.signOut()
